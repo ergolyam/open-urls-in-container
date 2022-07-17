7f2c9cfcc4ca3e7abc436c4448b416d4bda3321a
Hugh Blackall <hughblackall@gmail.com>
2022-07-18 01:07:28 +1000

    Convert extension to rely on webpack build, add Vue app for settings page

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+	entry: {
+		settings: './src/settings/settings.js',
+	},
+	output: {
+		filename: '[name]/[name].bundle.js',
+		path: path.resolve(__dirname, 'build/webpack'),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'settings/settings.html',
+			template: 'src/settings/settings.html',
+		}),
+		new VueLoaderPlugin(),
+		new CopyPlugin({
+			patterns: [{ from: 'src/background', to: 'background' }, { from: 'src/extension' }],
+		}),
+	],
+	watchOptions: {
+		ignored: /node_modules/,
+	},
+}
